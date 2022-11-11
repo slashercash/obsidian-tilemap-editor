29ddc4ba14bb1b020b1d886629ecaa78e437ebf7
David Zweili <david.zweili@gmail.com>
2022-11-11 20:25:07 +0100

    Display raw html content in TileView

---
+++ TileView.tsx
@@ -0,0 +1,52 @@
+import { WorkspaceLeaf, FileView, TFile, Notice } from 'obsidian'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createRoot, Root } from 'react-dom/client'
+
+export const TILE_FILE_EXTENSIONS = ['html']
+export const VIEW_TYPE_TILE = 'tile-view'
+
+export class TileView extends FileView {
+  allowNoFile: false
+
+  private editToggleAction_Element: HTMLElement
+  private root: Root
+
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf)
+  }
+
+  getViewType(): string {
+    return VIEW_TYPE_TILE
+  }
+
+  getIcon(): string {
+    return 'dice'
+  }
+
+  async onload(): Promise<void> {
+    this.root = createRoot(this.containerEl.children[1])
+
+    this.editToggleAction_Element = this.addAction(
+      'pencil',
+      'Current view: reading\nClick to edit',
+      () => new Notice('Not implemented')
+    )
+  }
+
+  async onLoadFile(file: TFile): Promise<void> {
+    const fileContent = await this.app.vault.read(file)
+    this.root.render(<Display fileContent={fileContent} />)
+  }
+
+  async onunload() {
+    this.editToggleAction_Element.remove()
+    this.root.unmount()
+  }
+}
+
+interface IDisplay {
+  fileContent: string
+}
+
+const Display = ({ fileContent }: IDisplay) => <p>{fileContent}</p>
--- main.ts
+++ main.ts
@@ -1,41 +1,25 @@
 import { Menu, MenuItem, Notice, Plugin } from 'obsidian'
-import { ExampleView, VIEW_TYPE_EXAMPLE } from './ExampleView'
-import { TestView, VIEW_TYPE_HTML, HTML_FILE_EXTENSIONS } from './TestView'
+import { TileView, VIEW_TYPE_TILE, TILE_FILE_EXTENSIONS } from './TileView'
 
 export default class MyPlugin extends Plugin {
   async onload() {
-    this.addRibbonIcon('dice', 'Open example view', async () => {
-      this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE)
+    this.registerView(VIEW_TYPE_TILE, (leaf) => new TileView(leaf))
 
-      await this.app.workspace.getRightLeaf(false).setViewState({
-        type: VIEW_TYPE_EXAMPLE,
-        active: true
+    this.registerEvent(
+      this.app.workspace.on('file-menu', (menu: Menu) => {
+        menu.addItem((item: MenuItem) => {
+          item
+            .setTitle('Create new Tilemap')
+            .setIcon('dice')
+            .onClick(() => new Notice('Not implemented'))
+        })
       })
+    )
 
-      this.app.workspace.revealLeaf(this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0])
-    })
-
-    this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf))
-
-    this.registerView(VIEW_TYPE_HTML, (leaf) => new TestView(leaf))
-
-    this.registerEvent(this.app.workspace.on('file-menu', fileMenuHandlerCreateNewTilemap))
-
-    try {
-      this.registerExtensions(HTML_FILE_EXTENSIONS, VIEW_TYPE_HTML)
-    } catch (error) {}
+    this.registerExtensions(TILE_FILE_EXTENSIONS, VIEW_TYPE_TILE)
   }
 
   async onunload() {
-    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE)
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_TILE)
   }
-}
-
-const fileMenuHandlerCreateNewTilemap = (menu: Menu) => {
-  menu.addItem((item: MenuItem) => {
-    item
-      .setTitle('Create new Tilemap')
-      .setIcon('dice')
-      .onClick(() => new Notice('Not implemented'))
-  })
 }

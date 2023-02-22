a0c676b7441cd28bef3727f7df1d62db46df22b0
David Zweili <david.zweili@gmail.com>
2023-02-22 20:48:57 +0100

    Outsouce Parser

---
+++ src/Parser.ts
@@ -0,0 +1,138 @@
+import { Tilemap, TilemapCell, TilemapElement, TilemapRow } from './types/tilemap'
+
+export class Parser {
+  static stringToTilemap(fileContent: string): Tilemap {
+    const htmlDoc: Document = new DOMParser().parseFromString(fileContent, 'text/html')
+    const tilemapElement = htmlDoc.getElementById('tilemap')
+
+    if (tilemapElement === null) {
+      throw new Error('could not read tilemap')
+    }
+
+    return { rows: Array.from(tilemapElement.children).map(parseRow) }
+  }
+
+  static tilemapToString(tilemap: Tilemap): string {
+    const html = tilemapToHtml(tilemap)
+
+    const htmlString = htmlToString(html)
+
+    return htmlString.replace('<style></style>', `<style>${cssStyle}</style>`)
+  }
+}
+
+function parseRow(row: Element): TilemapRow {
+  const cells: ReadonlyArray<TilemapCell> = Array.from(row.children).map(parseCell)
+  return { cells }
+}
+
+function parseCell(cell: Element): TilemapCell {
+  const elements: ReadonlyArray<TilemapElement> = Array.from(cell.children).map(parseElement)
+  return { elements }
+}
+
+function parseElement(element: Element): TilemapElement {
+  return {
+    className: element.className
+  }
+}
+
+function tilemapToHtml(tilemap: Tilemap): HTMLElement {
+  const doc = new Document()
+
+  const div = tilemapToDiv(doc, tilemap)
+  const html = divToHtml(doc, div)
+
+  return html
+}
+
+function tilemapToDiv(doc: Document, tilemap: Tilemap): HTMLDivElement {
+  const div: HTMLDivElement = doc.createElement('div')
+  div.setAttribute('id', 'tilemap')
+
+  tilemap.rows.forEach((row) => {
+    const rowElement = doc.createElement('div')
+    rowElement.setAttribute('class', 'tilemap-row')
+
+    row.cells.forEach((cell) => {
+      const cellElement = doc.createElement('div')
+      cellElement.setAttribute('class', 'tilemap-cell')
+      cellElement.appendText('')
+
+      cell.elements.forEach((element) => {
+        const elementElement = doc.createElement('div')
+        elementElement.setAttribute('class', element.className)
+        elementElement.appendText('')
+
+        cellElement.appendChild(elementElement)
+      })
+      rowElement.appendChild(cellElement)
+    })
+    div.appendChild(rowElement)
+  })
+
+  return div
+}
+
+function divToHtml(doc: Document, div: HTMLDivElement): HTMLHtmlElement {
+  const html = doc.createElement('html')
+  const body = doc.createElement('body')
+  const main = doc.createElement('main')
+  const style = doc.createElement('style')
+  style.appendText('')
+
+  main.appendChild(div)
+  body.appendChild(main)
+  html.appendChild(body)
+  html.appendChild(style)
+
+  return html
+}
+
+const cssStyle = `
+  main {
+    display: flex;
+    justify-content: center;
+  }
+  .tilemap-row {
+    display: flex;
+  }
+  .tilemap-cell {
+    display: grid;
+    height: 30px;
+    width: 30px;
+    box-shadow: inset 0 0 0 1px lightgray;
+  }
+  .tilemap-cell > div {
+    grid-row-start: 1;
+    grid-column-start: 1;
+  }
+  .tile {
+    background-color: rgb(93, 0, 255);
+    box-shadow: inset 0 0 0 1px black;
+  }
+`
+
+function htmlToString(html: HTMLElement): string {
+  return format(html).innerHTML
+}
+
+function format(node: Element, level: number = 0) {
+  var indentBefore = new Array(level++ + 1).join('  '),
+    indentAfter = new Array(level - 1).join('  '),
+    textNode
+
+  for (var i = 0; i < node.children.length; i++) {
+    textNode = document.createTextNode('\n' + indentBefore)
+    node.insertBefore(textNode, node.children[i])
+
+    format(node.children[i], level)
+
+    if (node.lastElementChild == node.children[i]) {
+      textNode = document.createTextNode('\n' + indentAfter)
+      node.appendChild(textNode)
+    }
+  }
+
+  return node
+}
--- src/TilemapEditorView.ts
+++ src/TilemapEditorView.ts
@@ -1,7 +1,8 @@
 import { WorkspaceLeaf, FileView, Notice } from 'obsidian'
 import { createRoot, Root } from 'react-dom/client'
 import Editor from './editor/Editor'
-import { Tilemap, TilemapCell, TilemapElement, TilemapRow } from './types/tilemap'
+import { Tilemap } from './types/tilemap'
+import { Parser } from './Parser'
 
 export const TILE_FILE_EXTENSIONS = ['html']
 export const VIEW_TYPE_TILE = 'tile-view'
@@ -65,7 +66,7 @@ export class TilemapEditorView extends FileView {
     )
 
     this.saveAction_Element = this.addAction('checkmark', 'Save', () => {
-      const newFileContent = parseFileContent(this.tilemap)
+      const newFileContent = Parser.tilemapToString(this.tilemap)
       this.app.vault.modify(this.file, newFileContent)
       new Notice('File saved')
     })
@@ -76,7 +77,7 @@ export class TilemapEditorView extends FileView {
 
   async onLoadFile(): Promise<void> {
     const fileContent = await this.app.vault.read(this.file)
-    this.tilemap = parseTilemap(fileContent)
+    this.tilemap = Parser.stringToTilemap(fileContent)
     this.renderEditor()
   }
 
@@ -86,122 +87,4 @@ export class TilemapEditorView extends FileView {
     this.saveAction_Element.remove()
     this.root.unmount()
   }
-}
-
-function parseTilemap(view: string): Tilemap {
-  const htmlDoc = new DOMParser().parseFromString(view, 'text/html')
-  const tm = htmlDoc.getElementById('tilemap')
-
-  if (tm === null) {
-    throw new Error('could not read tilemap')
-  }
-
-  const rows: ReadonlyArray<TilemapRow> = Array.from(tm.children).map(parseRow)
-
-  const tilemap: Tilemap = { rows }
-
-  return tilemap
-}
-
-function parseRow(row: Element): TilemapRow {
-  const cells: ReadonlyArray<TilemapCell> = Array.from(row.children).map(parseCell)
-  return { cells }
-}
-
-function parseCell(cell: Element): TilemapCell {
-  const elements: ReadonlyArray<TilemapElement> = Array.from(cell.children).map(parseElement)
-  return { elements }
-}
-
-function parseElement(element: Element): TilemapElement {
-  return {
-    className: element.className
-  }
-}
-
-function parseFileContent(tilemap: Tilemap): string {
-  const htmlDoc = new Document()
-
-  const html = htmlDoc.createElement('html')
-  const body = htmlDoc.createElement('body')
-  const style = htmlDoc.createElement('style')
-  const main = htmlDoc.createElement('main')
-  const div = htmlDoc.createElement('div')
-  div.setAttribute('id', 'tilemap')
-
-  style.appendText('')
-  main.appendChild(div)
-  body.appendChild(main)
-
-  html.appendChild(style)
-  html.appendChild(body)
-
-  htmlDoc.appendChild(html)
-
-  tilemap.rows.forEach((row) => {
-    const rowElement = htmlDoc.createElement('div')
-    rowElement.setAttribute('class', 'tilemap-row')
-
-    row.cells.forEach((cell) => {
-      const cellElement = htmlDoc.createElement('div')
-      cellElement.setAttribute('class', 'tilemap-cell')
-      cellElement.appendText('')
-
-      cell.elements.forEach((element) => {
-        const elementElement = htmlDoc.createElement('div')
-        elementElement.setAttribute('class', element.className)
-        elementElement.appendText('')
-
-        cellElement.appendChild(elementElement)
-      })
-      rowElement.appendChild(cellElement)
-    })
-    div.appendChild(rowElement)
-  })
-
-  return format(htmlDoc.documentElement).innerHTML.replace('<style></style>', `<style>${cssStyle}</style>`)
-}
-
-const cssStyle = `
-  main {
-    display: flex;
-    justify-content: center;
-  }
-  .tilemap-row {
-    display: flex;
-  }
-  .tilemap-cell {
-    display: grid;
-    height: 30px;
-    width: 30px;
-    box-shadow: inset 0 0 0 1px lightgray;
-  }
-  .tilemap-cell > div {
-    grid-row-start: 1;
-    grid-column-start: 1;
-  }
-  .tile {
-    background-color: rgb(93, 0, 255);
-    box-shadow: inset 0 0 0 1px black;
-  }
-`
-
-function format(node: Element, level: number = 0) {
-  var indentBefore = new Array(level++ + 1).join('  '),
-    indentAfter = new Array(level - 1).join('  '),
-    textNode
-
-  for (var i = 0; i < node.children.length; i++) {
-    textNode = document.createTextNode('\n' + indentBefore)
-    node.insertBefore(textNode, node.children[i])
-
-    format(node.children[i], level)
-
-    if (node.lastElementChild == node.children[i]) {
-      textNode = document.createTextNode('\n' + indentAfter)
-      node.appendChild(textNode)
-    }
-  }
-
-  return node
 }

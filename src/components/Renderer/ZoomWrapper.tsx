4a4c7cbe30531d741659893302baa1ba605f0bc6
David Zweili <david.zweili@gmail.com>
2023-08-27 19:59:56 +0200

    Implement changing scroll position via click and drag

--- src/components/Renderer/ZoomWrapper.tsx
+++ src/components/Renderer/ZoomWrapper.tsx
@@ -17,6 +17,8 @@ export const ZoomWrapper: FC<ZoomWrapperProps> = ({ children, tilemapRendererRef
     setTilemapRendererDiv(tilemapRendererRef.current)
   }, [])
 
+  addMoveEvents(tilemapRendererDiv)
+
   const onTouchEvent = (e: TouchEvent) => {
     const tA = e.touches[0]
     const tB = e.touches[1]
@@ -66,4 +68,53 @@ function getDistance(x1: number, y1: number, x2: number, y2: number) {
   const y = x2 - x1
   const x = y2 - y1
   return Math.sqrt(x * x + y * y)
+}
+
+function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
+  if (!tilemapRendererDiv) {
+    return
+  }
+
+  let mouseDown = false
+  let moving = false
+  let startX: number
+  let startY: number
+  let scrollLeft: number
+  let scrollTop: number
+
+  const startDragging = (e: MouseEvent) => {
+    mouseDown = true
+    startX = e.pageX - tilemapRendererDiv.offsetLeft
+    startY = e.pageY - tilemapRendererDiv.offsetTop
+    scrollLeft = tilemapRendererDiv.scrollLeft
+    scrollTop = tilemapRendererDiv.scrollTop
+  }
+
+  const stopDragging = (e: MouseEvent) => {
+    if (moving) {
+      e.stopPropagation()
+    }
+
+    mouseDown = false
+    moving = false
+  }
+
+  const mouseMove = (e: MouseEvent) => {
+    if (!mouseDown) {
+      return
+    }
+
+    const moveDistanceX = e.pageX - tilemapRendererDiv.offsetLeft - startX
+    const moveDistanceY = e.pageY - tilemapRendererDiv.offsetTop - startY
+
+    tilemapRendererDiv.scrollLeft = scrollLeft - moveDistanceX
+    tilemapRendererDiv.scrollTop = scrollTop - moveDistanceY
+
+    moving = moving || Math.abs(moveDistanceX) > 5 || Math.abs(moveDistanceY) > 5
+  }
+
+  tilemapRendererDiv.addEventListener('mousedown', startDragging)
+  tilemapRendererDiv.addEventListener('click', stopDragging)
+  tilemapRendererDiv.addEventListener('mouseleave', stopDragging)
+  tilemapRendererDiv.addEventListener('mousemove', mouseMove)
 }

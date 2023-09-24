8981a195d3b0c09527d96b7c4b2d9c5ea805f3ff
David Zweili <david.zweili@gmail.com>
2023-09-24 11:18:43 +0200

    Fix dragging with rightclick

--- src/components/Renderer/ZoomWrapper.tsx
+++ src/components/Renderer/ZoomWrapper.tsx
@@ -75,7 +75,7 @@ function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
     return
   }
 
-  let mouseDown = false
+  let dragging = false
   let moving = false
   let startX: number
   let startY: number
@@ -83,7 +83,7 @@ function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
   let scrollTop: number
 
   const startDragging = (e: MouseEvent) => {
-    mouseDown = true
+    dragging = true
     startX = e.pageX - tilemapRendererDiv.offsetLeft
     startY = e.pageY - tilemapRendererDiv.offsetTop
     scrollLeft = tilemapRendererDiv.scrollLeft
@@ -95,12 +95,12 @@ function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
       e.stopPropagation()
     }
 
-    mouseDown = false
+    dragging = false
     moving = false
   }
 
   const mouseMove = (e: MouseEvent) => {
-    if (!mouseDown) {
+    if (!dragging) {
       return
     }
 
@@ -113,7 +113,7 @@ function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
     moving = moving || Math.abs(moveDistanceX) > 5 || Math.abs(moveDistanceY) > 5
   }
 
-  tilemapRendererDiv.addEventListener('mousedown', startDragging)
+  tilemapRendererDiv.addEventListener('mousedown', (e) => e.button == 0 && startDragging(e))
   tilemapRendererDiv.addEventListener('click', stopDragging)
   tilemapRendererDiv.addEventListener('mouseleave', stopDragging)
   tilemapRendererDiv.addEventListener('mousemove', mouseMove)

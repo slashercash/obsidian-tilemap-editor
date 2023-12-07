d4cc33c9dfb1f9c69d80dfe74f8f53cbba731e2f
David Zweili <david.zweili@gmail.com>
2023-12-07 16:16:44 +0100

    Add Renderer

--- src/TilemapEditor.ts
+++ src/TilemapEditor.ts
@@ -1,14 +1,17 @@
 import type { TilemapMetadata } from 'TilemapEditorView'
+import { Renderer } from 'components/Renderer'
 import { Toolbar } from 'components/Toolbar'
 
 export class TilemapEditor {
   private readonly tilemapEditor: HTMLElement
   private readonly toolbar: Toolbar
+  private readonly renderer: Renderer
 
   constructor(rootElement: HTMLElement) {
     this.tilemapEditor = document.createElement('div')
     this.tilemapEditor.className = 'tilemap-editor'
     this.toolbar = new Toolbar(this.tilemapEditor)
+    this.renderer = new Renderer(this.tilemapEditor)
     rootElement.appendChild(this.tilemapEditor)
   }
 
---
+++ src/components/Renderer.ts
@@ -0,0 +1,65 @@
+export class Renderer {
+  private readonly renderer: HTMLDivElement
+  private readonly styleElement: HTMLStyleElement
+
+  constructor(rootElement: HTMLElement) {
+    this.renderer = document.createElement('div')
+    this.renderer.className = 'tilemap-renderer'
+    addMoveEvents(this.renderer)
+
+    this.styleElement = document.createElement('style')
+    this.styleElement.innerText = `.view-content-tilemap-editor .tilemap-cell { width:${30}px;height:${30}px; }`
+
+    rootElement.appendChild(this.renderer)
+    rootElement.appendChild(this.styleElement)
+  }
+}
+
+function addMoveEvents(tilemapRendererDiv: HTMLDivElement | null) {
+  if (!tilemapRendererDiv) {
+    return
+  }
+
+  let dragging = false
+  let moving = false
+  let startX: number
+  let startY: number
+  let scrollLeft: number
+  let scrollTop: number
+
+  const startDragging = (e: MouseEvent) => {
+    dragging = true
+    startX = e.pageX - tilemapRendererDiv.offsetLeft
+    startY = e.pageY - tilemapRendererDiv.offsetTop
+    scrollLeft = tilemapRendererDiv.scrollLeft
+    scrollTop = tilemapRendererDiv.scrollTop
+  }
+
+  const stopDragging = (e: MouseEvent) => {
+    if (moving) {
+      e.stopPropagation()
+    }
+
+    dragging = false
+    moving = false
+  }
+
+  const mouseMove = (e: MouseEvent) => {
+    if (!dragging) {
+      return
+    }
+
+    const moveDistanceX = e.pageX - tilemapRendererDiv.offsetLeft - startX
+    const moveDistanceY = e.pageY - tilemapRendererDiv.offsetTop - startY
+
+    tilemapRendererDiv.scrollLeft = scrollLeft - moveDistanceX
+    tilemapRendererDiv.scrollTop = scrollTop - moveDistanceY
+
+    moving = moving || Math.abs(moveDistanceX) > 5 || Math.abs(moveDistanceY) > 5
+  }
+
+  tilemapRendererDiv.addEventListener('mousedown', (e) => e.button == 0 && startDragging(e))
+  tilemapRendererDiv.addEventListener('click', stopDragging)
+  tilemapRendererDiv.addEventListener('mouseleave', stopDragging)
+  tilemapRendererDiv.addEventListener('mousemove', mouseMove)
+}
